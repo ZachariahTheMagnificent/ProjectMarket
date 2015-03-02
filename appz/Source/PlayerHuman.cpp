ef635704c5c95aae3ed84f63b2c729eb2a509bc1
Pyrolf <pyroflame11@gmail.com>
2015-03-02 20:27:27 +0800

    Acccurate release

--- appz/Source/PlayerHuman.cpp
+++ appz/Source/PlayerHuman.cpp
@@ -90,7 +90,12 @@ void PlayerHuman::ReleaseTrolley(const Vector3& TrolleyCurrentPos)
 	if(isHoldingTrolley == true)
 	{
 		trolley->SetParentAs(main);
-		trolley->transform.translate = TrolleyCurrentPos;
+		trolley->transform.rotate.Set(0,0,0);
+		trolley->selfTransform.translate.Set(0,0,0);
+		trolley->transform.translate.Set(0,0,0);
+		trolley->transform.rotate.y = body->transform.rotate.y;
+		trolley->transform.translate += TrolleyCurrentPos + body->transform.rotate.MatrixY() * Vector3(5,-3,0);
+
 		isHoldingTrolley = false;
 	}
 }
